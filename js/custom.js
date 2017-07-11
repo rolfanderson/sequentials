e69312741054c14d45076695a14eb325598642e5
Rolf Anderson <rolf.k.anderson@gmail.com>
2017-07-11 15:43:41 -0600

    update

---
+++ js/custom.js
@@ -0,0 +1,20 @@
+$(document).ready(function () {
+
+    var menu = $('.navbar');
+    var origOffsetY = menu.offset().top;
+
+    function scroll() {
+        if ($(window).scrollTop() >= origOffsetY) {
+            $('.navbar').addClass('navbar-fixed-top');
+            $('.content').addClass('menu-padding');
+        } else {
+            $('.navbar').removeClass('navbar-fixed-top');
+            $('.content').removeClass('menu-padding');
+        }
+
+
+    }
+
+    document.onscroll = scroll;
+
+});
