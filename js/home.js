b2b465ba5d0f989303073db7cdd343d044b20340
Rolf Anderson <rolf.k.anderson@gmail.com>
2017-07-24 13:51:00 -0600

    material update

--- js/home.js
+++ js/home.js
@@ -1,3 +1,9 @@
 /*$(window).on('load',function(){
     $('#disclaimer').modal('show');
 });*/
+
+jQuery(document).ready(function($) {
+    $(".clickable-row").click(function() {
+        window.location = $(this).data("href");
+    });
+});
